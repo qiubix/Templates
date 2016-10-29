ae482f1f786260c8212f5b378ab65cebfd2b165f
qiubix <qiubix@gmail.com>
2016-10-29 23:21:45 +0200

    Add sample java projects

---
+++ java/maven-simple-project/src/test/java/com/qiubix/draft/AppTest.java
@@ -0,0 +1,15 @@
+package com.qiubix.draft;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class AppTest
+{
+
+  @Test
+  public void testApp() {
+    assertThat(1, equalTo(1));
+  }
+}
